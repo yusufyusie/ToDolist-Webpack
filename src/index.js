23bd3f09debd797b38996b807b41fc9b0ed64dab
yusufyusie <yesuffenta023@gmail.com>
2023-07-14 19:30:38 +0300

    refactor to make correction

--- src/index.js
+++ src/index.js
@@ -1,43 +1,4 @@
 // import './style.css';
-import ToDoList from './modules/toDoList.js';
-import UI from './modules/ui.js';
-import LocalStorage from './modules/localStorage.js';
-import CompleteToDoList from './modules/CompleteToDoList.js';
+import addToDoLists from './modules/ui.js';
 
-document
-  .querySelector('.fa-arrows-rotate')
-  .addEventListener('click', () => window.location.reload());
-
-document.addEventListener('DOMContentLoaded', () => {
-  const listContainer = document.querySelector('.add-to-list');
-  listContainer.innerHTML = `<p class = "error-message">*Error</p>
-        <form class= "add-to-list" action="">
-            <input type="text" class="text" placeholder="Add to list ..." required />
-            <i class="fa-solid fa-arrow-right-to-bracket"></i>
-        </form>
-        `;
-  UI.showToDoLists();
-  UI.removeToDoLists();
-  CompleteToDoList.compToDoList();
-  CompleteToDoList.clearComp();
-});
-
-document.querySelector('.add-to-list').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const inputText = document.querySelector('.text');
-  const todolists = LocalStorage.getToDoLists();
-  const description = inputText.value;
-  const index = todolists.length + 1;
-  const completed = false;
-
-  const todotask = new ToDoList(description, completed, index);
-
-  UI.addToDoLists(todotask);
-
-  LocalStorage.addToDoLists(todotask);
-
-  UI.clearFields();
-
-  window.location.reload();
-});
+document.onload(addToDoLists());
--- src/modules/toDoList.js
+++
@@ -1,9 +0,0 @@
-class ToDoList {
-  constructor(description, completed, index) {
-    this.description = description;
-    this.completed = completed;
-    this.index = index;
-  }
-}
-
-export default ToDoList;
