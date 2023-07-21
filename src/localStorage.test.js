045e183f3d7b7cbc9f8b2e8a21dfa48e3905b351
yusufyusie <yesuffenta023@gmail.com>
2023-07-21 15:14:44 +0300

    Add unit testing using jest start

--- src/localStorage.test.js
+++ src/localStorage.test.js
@@ -3,31 +3,4 @@
 const UI = require('./modules/ui.js');
 
 describe('addToDoLists function', () => {
-  let localStorageMock = {};
-  beforeEach(() => {
-    localStorageMock = {};
-    global.localStorage = {
-      getItem: (key) => localStorageMock[key],
-      setItem: (key, value) => {
-        localStorageMock[key] = value;
-      },
-    };
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    test('should add a new item to the todoList', () => {
-      localStorageMock.todoListItems = '[]';
-
-      const listContainer = document.querySelector('.add-to-list');
-      listContainer.innerHTML = `<p class = "error-message">*Error</p>
-        <form class= "add-to-list" action="">
-            <input type="text" class="text" placeholder="Add to list ..." required />
-            <i class="fa-solid fa-arrow-right-to-bracket"></i>
-        </form>
-        `;
-      UI.showToDoLists();
-      UI.removeToDoLists();
-      UI.updateDesc();
-    });
-  });
 });
